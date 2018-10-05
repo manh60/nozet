85e7d9761b498df23c3e1efbdc7aa589f417c391
rootx <manhnguyen@it-s.vn>
2018-10-06 02:17:23 +0700

    hoanthanhadidasblog1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'home/index'
- 
-  resources :products
- 
-  root 'home#index'
+	
+	root 'home#index'
+	
+	resources :products do
+		resources :comments
+	end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
