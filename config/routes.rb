fb83f6eb5476ede7a76368ae5aadba1eaa33b3f2
rootx <manhnguyen@it-s.vn>
2018-10-08 10:28:54 +0700

    0 892018

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
 	resources :products do
 		resources :comments
 	end
+
+	require 'sidekiq/web'
+	require 'sidekiq-scheduler/web'
+mount Sidekiq::Web => '/sidekiq'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
